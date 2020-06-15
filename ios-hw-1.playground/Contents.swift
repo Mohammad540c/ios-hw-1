4be2bf8222665e0f9b40bd1290d7b1bf8834ef08
Mohammad540c <66434093+Mohammad540c@users.noreply.github.com>
2020-06-15 23:30:36 +0300

    finshed

--- ios-hw-1.playground/Contents.swift
+++ ios-hw-1.playground/Contents.swift
@@ -5,12 +5,13 @@
  
  */
 
-let name = "Ahmad"
+var name = "Ahmad"
 
 name = "Salem"
 
 
-var myWeight = 60
+
+var myWeight = 60.65
 var FriendWeight = 80.5
 var height = 1.5
 
@@ -38,33 +39,42 @@ var bmi = myWeight * (height * height)
  */
 
 //Person 1
-var firstName1 = ""
-var lastName1 = ""
-var email1 = ""
-var phoneNumber1 = ""
-var age1 = 0
-var country1 = ""
-var area1 = ""
-var password = ""
-var isKuwaiti = false
+var firstName1 = "Ahmad"
+var lastName1 = "Kandari"
+var email1 = "ahmed002aca@hotmail.com"
+var phoneNumber1 = "99365243"
+var age1 = 15
+var country1 = "Kuwait"
+var area1 = "Jabria"
+var password = "99204040"
+var isKuwaiti = true
 
 //Person 2
 
-
-
-
-
+var firstNames2 = "Jaber"
+var lastName2 = "Mohammad"
+var email2 = "jabermohamm05@hotmail.com"
+var phoneNumber2 = "94774363"
+var age2 = 16
+var country2 = "Kuwait"
+var area2 = "Zahra"
+var password2 = "jab53734"
+var isKuwaiti2 = true
 
 
 
 
 
 //Person 3
-
-
-
-
-
+var firstName3 = "khald"
+var lastName3 = "majed"
+var email3 = "khaled6895621@hotmail.com"
+var phoneNumber3 = "83648374"
+var age3 = 15
+var country3 = "Kuwait"
+var area3 = "Jahra"
+var password3 = "649khald"
+var isKuwaiti3 = true
 
 
 
@@ -82,3 +92,72 @@ var isKuwaiti = false
 
  
  */
+
+firstName1 == firstNames2
+lastName1 != lastName2
+firstName1 != firstNames2
+lastName1 == lastName2
+
+
+firstName1 == firstName3
+firstName1 != firstName3
+lastName1 == lastName3
+lastName1 != lastName3
+
+firstNames2 == firstName3
+firstNames2 != firstName3
+lastName2 == lastName3
+lastName1 != lastName3
+
+
+age1 == age2
+age1 != age2
+
+age1 == age3
+age1 != age3
+
+age2 == age3
+age2 != age3
+
+area1 == area2
+area1 != area2
+
+area1 == area3
+area1 != area3
+
+area2 == area3
+area2 != area3
+
+
+age1 > 18
+
+age2 > 18
+
+age3 > 18
+
+
+age1 == 18
+
+age2 == 18
+
+age3 == 18
+
+
+age1 < 18
+
+age2 < 18
+
+age3 < 18
+
+
+age1 != 18
+
+age2 != 18
+
+age3 != 18
+
+
+
+
+
+
